e11ba623b37e856d838c79924b647c06fce67106
Lars Rudbeck Bak <larsbakprivatemail@gmail.com>
2017-10-19 12:48:22 +0200

    sync

--- src/Test.java
+++ src/Test.java
@@ -1,54 +1,75 @@
+import com.sun.tools.classfile.Attribute;
 import it.unisa.dia.gas.jpbc.*;
+import it.unisa.dia.gas.plaf.jpbc.field.curve.CurveElement;
 import it.unisa.dia.gas.plaf.jpbc.pairing.PairingFactory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        int zeros = 0;
-        for (int i = 0; i < 10; i++) {
-            SecureRandom random = new SecureRandom();
+        String input = "hello@world.dk";
+        String filename = "test.properties";
 
-            PairingParametersGenerator parametersGenerator = new OurTypeACurveGenerator(random, 256, 1024, true);
-            PairingParameters params = parametersGenerator.generate();
+
+        for (int i = 0; i < 100; i++) {
+
+            generate(filename);
+
+            PairingFactory.getInstance().setUsePBCWhenPossible(true);
+            Pairing pairing = PairingFactory.getPairing(filename);
 
             // Step 1
-            Pairing pairing = PairingFactory.getPairing(params, random);
 
             // Step 2
             Element s = pairing.getZr().newRandomElement();
-            while (s.isZero()) { s = pairing.getZr().newRandomElement(); }
+            if (s.isZero() || s.isOne()) { throw new RuntimeException("s is not generator"); }
 
-            Element p = getGenerator(pairing.getG1());
-            Element pPub = p.mulZn(s);
+            Element p = pairing.getG1().newRandomElement();
+            if (p.isZero() || p.isOne()) { throw new RuntimeException("p is not generator"); }
 
-            System.out.println("s: " + s);
-            System.out.println("p: " + p);
-            System.out.println("pPub: " + pPub);
+            Element sp = p.mulZn(s);
+            if (sp.isZero() || sp.isOne()) { throw new RuntimeException("ill result for sp"); }
 
-            // TODO: Why is pPub always zero ???
-            if (pPub.isZero()) {
-                zeros += 1;
+            // TODO: Why cant we convert curve element to bigInteger?
+
+            MessageDigest sha256 = null;
+            try {
+                sha256 = MessageDigest.getInstance("SHA-256");
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+                System.exit(-1);
             }
-        }
-        System.out.println("Zeros: " +zeros);
 
+            BigInteger hash = new BigInteger(sha256.digest(input.getBytes()));
+
+            // TODO: Convert hash to G1 element
+
+        }
     }
 
-    public static boolean isGenerator(Element e, BigInteger order) {
-        return !e.isZero() && e.pow(order).isZero();
-    }
+    private static void generate(String filename) {
 
-    public static Element getGenerator(Field f) {
-        Element g = f.newRandomElement();
-        while (!isGenerator(g, f.getOrder())) {
-            //System.out.println("Element is not generator (not expected)");
-            g = f.newRandomElement();
+        SecureRandom random = new SecureRandom();
+        PairingParametersGenerator parametersGenerator = new OurTypeACurveGenerator(random, 256, 1024, false);
+        PairingParameters params = parametersGenerator.generate();
+
+        File old = new File(filename);
+        boolean deleted = old.delete();
+        System.out.println(String.format("%s %s", filename, deleted ? "overwritten" : "created"));
+
+        try (PrintWriter out = new PrintWriter(filename)) {
+            out.print(params.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        return g;
     }
 
 }
